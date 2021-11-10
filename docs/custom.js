97c4617eb27a68c10beeac71d10d45f11d06b804
tejaswigowda <tejaswi@foxyninjastudios.com>
2021-11-09 22:33:33 -0700

    ac

--- docs/custom.js
+++ docs/custom.js
@@ -23,18 +23,33 @@ function handleWSMessage(obj)
       var q = new Quaternion(obj.x, obj.y, obj.z, obj.w);
       var qC = new Quaternion(mac2Bones[obj.id].calibration.x,mac2Bones[obj.id].calibration.y,mac2Bones[obj.id].calibration.z,mac2Bones[obj.id].calibration.w).inverse()
       var qR = q.mul(qC);
+      var qF;
+
 
       switch(bone){
-        case 'LeftForeArm':
-          x.quaternion.set(qR.y, -qR.x, -qR.z, qR.w);
+        case 'LeftArm':
+          qF = qR;
+          x.quaternion.set(-qF.y, qF.x, -qF.z, qF.w);
           break;
        
-        case 'LeftArm':
-          x.quaternion.set(qR.y, -qR.x, -qR.z, qR.w);
+        case 'LeftForeArm':
+         // console.log(bone, obj,qR);
+         /*
+          var q = new Quaternion(qR.x, qR.y, qR.z, qR.w);
+          var qC = new Quaternion(mac2Bones["LeftArm"].actual.x,mac2Bones["LeftArm"].actual.y,mac2Bones["LeftArm"].actual.z,mac2Bones["LeftArm"].calibration.w).inverse()
+          qF = q.mul(qC);
+          */
+          qF = qR;
+          x.quaternion.set(qF.z, -qF.x, qF.y, qF.w);
           break;
        
         default:
-         x.quaternion.set(qR.y, -qR.x, -qR.z, qR.w);
+         qF = qR;
+         x.quaternion.set(qF.y, -qF.x, -qF.z, qF.w);
          break;
       }
+      mac2Bones[obj.id].actual.x = qF.x;
+      mac2Bones[obj.id].actual.y = qF.y;
+      mac2Bones[obj.id].actual.z = qF.z;
+      mac2Bones[obj.id].actual.w = qF.w;
 }
